bc698c0669f59e41cc74441d19684c147706121e
brownlock2203 <brandonmedehou2203@gmail.com>
2024-03-20 00:56:24 +0100

    adding tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app" class="bg-[#eee9e9] rounded-[16px] p-[20px] shadow-[0_0_12px_rgba(0, 0, 0, 0.1)] flex flex-col justify-center items-center">
+    <h1 class="text-[30px] mb-[1.5rem]">Chronomètre</h1>
+    <div class="flex flex-col items-center gap-[20px]">
+      <div class="timer text-[40px] bg-[#ffffff] p-[20px] rounded-[8px] shadow-[0_0_12px_rgba(0, 0, 0, 0.1)] animate-[pulse_1.5s_infinite_alternate]">
+        <span id="minutes">00</span>:
+        <span id="seconds">00</span>:
+        <span id="milliseconds">00</span>
+      </div>
+      <div class="flex gap-[1rem]">
+        <button id="startBtn">Commencer</button>
+        <button id="stopBtn">Arrêter</button>
+        <button id="pauseBtn">Pause</button>
+        <button id="resetBtn">réinitialiser</button>
+      </div>
+      <div class="w-[100%]">
+        <h2 class="text-[1.5rem]">Lap Timer</h2>
+        <ul id="laplist"></ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    
+  }
+</script>
 
-nav {
-  padding: 30px;
-}
+<style lang="scss" scoped>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .timer span {
+    width: 110px;
+    display: inline-block;
+    text-align: center;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  button {
+    padding: 0.75rem 1.5rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    background-color: #f25191;
+    color: #ffffff;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  button:hover {
+    background-color: #d72a76;
+  }
+
+  // ul {
+  //   list-style-type: none;
+  //   padding: 0;
+  //   margin: 0;
+  //   overflow-y: auto;
+  //   max-height: 200px;
+  //   background-color: #ffffff;
+  //   border-radius: 8px;
+  //   box-shadow: 0 0 12px rgba(0, 0, 0, 0.1);
+  // }
+
+  // li {
+  //   padding: 1rem;
+  //   border-bottom: 1px solid #e0e0e0;
+  // }
+
+  // li:last-child {
+  //   border-bottom: none;
+  // }
+
+  // li span {
+  //   font-weight: 700;
+  // }
+
 </style>
